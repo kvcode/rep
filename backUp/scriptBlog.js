3491e50e7625280487a539e4f3b66296f0285164
kvcode <kvcode@users.noreply.github.com>
2020-05-24 13:00:42 +0300

    Update scriptBlog.js

--- backUp/scriptBlog.js
+++ backUp/scriptBlog.js
@@ -7,7 +7,7 @@ function loging(){
 var usn = document.getElementById('usn').value;
 var psw = document.getElementById("psw").value;
 if (usn == "banda" && psw == "banda") {
-    location.href = "https://kvcode.github.io/rep/main.html"
+    location.href = "https://kvcode.github.io/rep/blog/main.html"
     console.log("access granted");
 } else {
     location.reload();
